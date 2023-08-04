1d3b46cd6fe61dd0aa3bfb27a3b78fbd114c66bf
Sayan <banerjeesayan554@gmail.com>
2023-08-05 00:45:54 +0530

    Add to git

---
+++ main.py
@@ -0,0 +1,10 @@
+import win32com.client 
+speakers = win32com.client.Dispatch("SAPI.SpVoice")
+
+if __name__ == '__main__':
+    print("Welcome to robo_speake 1.0. made by sayan ")
+    while True:
+        x=input("Enter what you want to speake: ")
+        if x== "q":
+            break
+        speakers.speak(x)
